bac48f9e94859a6f1170c0508bd957be12851202
Michael Jivung <michael.jivung@gmail.com>
2023-04-19 16:57:49 +0100

    fix default constants

--- backend/src/config/common.js
+++ backend/src/config/common.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // The host of yggio-service-example backend
-// Will be localhost if backend is run locally
-const BACKEND_HOST = 'localhost';
+// Will be 127.0.0.1 if backend is run locally
+const BACKEND_HOST = '127.0.0.1';
 
 // The port used by the yggio-service-example backend
 // Should be the same as in frontend/src/constants/index.js
@@ -20,20 +20,20 @@ const FRONTEND_PORT = '9492';
 
 // The URL of the REST API of the Yggio you want to use
 // Should be the same as in frontend/src/constants/index.js
-// https://api.yggio-sandbox.sensative.net is a suitable test server and is set as default
-const YGGIO_API_URL = 'https://api.yggio-sandbox.sensative.net';
+// https://staging.yggio.net is a suitable test server and is set as default
+const YGGIO_API_URL = 'https://staging.yggio.net/';
 
 // The name of the client that is created
 // Can be set to anything
-const CLIENT_NAME = '';
+const CLIENT_NAME = 'micke-service-example-2';
 
 // Username of the user that creates the client
 // Can be set to anything
-const YGGIO_ACCOUNT_USERNAME = '';
+const YGGIO_ACCOUNT_USERNAME = 'test';
 
 // Password of the user that creates the client
 // Can be set to anything
-const YGGIO_ACCOUNT_PASSWORD = '';
+const YGGIO_ACCOUNT_PASSWORD = 'test';
 
 module.exports = {
   BACKEND_HOST,
